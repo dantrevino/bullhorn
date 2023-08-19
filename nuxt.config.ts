52196c70177b9dd6a11255c50c487f94d62b0286
Dan Trevino <dantrevino@gmail.com>
2023-08-19 02:21:17 -0500

    correct api endpoint and schema

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['nuxt-graphql-client', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+  'graphql-client': {
+    clients: {
+      default: {
+        schema: './schema.graphql',
+        host: 'https://usdc.api.test.deeplake.fi/graphql',
+        headers: {
+          'Authorization': '<foo>',
+        }
+      }
+    }
+  },  
   colorMode: {
     preference: 'light', // default theme
     dataValue: 'theme', // activate data-theme in <html> tag
